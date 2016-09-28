267a38b6cba93ca9d0fc82385fc0cd7e3b3bfc06
arstep <alek.arst@yandex.ru>
2016-09-28 14:01:29 +0400

    version 0.7

--- js/blog_js.js
+++ js/blog_js.js
@@ -1,10 +1,15 @@
-/**
- * Created by Администратор on 16.09.2016.
- */
 
 $(document).ready(function(){
+    parallax();
+    sliderRun(2000,1000,5000);
+    scrollEff();
+});
 
-    $('section[data-type="background"]').each(function(){
+
+
+
+function parallax() {
+    $('section[data-type="parallax"]').each(function(){
 
         var $bgobj = $(this);
 
@@ -17,7 +22,99 @@ $(document).ready(function(){
             $bgobj.css({ backgroundPosition: coords });
 
         });
+    });
+}
+
+
+function sliderRun(a, b, c) {
+    var SPEED = a;
+    var AFTER_SPEED = b;
+    var DELAY = c;
+    var container = $('#sliderBack');
+
+    stepOne();
+
+    function stepOne() {
+        container.append($('#sliderBack > img').first());
+
+        $('#sliderBack > img').last()
+            .css({
+                'opacity': '0',
+                'width': '1920px'
+            })
+            .delay(DELAY)
+            .animate({
+                opacity:'1'
+            },SPEED,stepTwo)
+    }
+
+    function stepTwo() {
+        container.append($('#sliderBack > img').first());
+
+        $('#sliderBack > img').last()
+            .css({
+                'opacity': '0',
+                'width': '0'
+            })
+            .delay(DELAY)
+            .animate({
+                width:'1920px',
+                opacity:'0.9'
+            },SPEED)
+            .animate({opacity:'1'},AFTER_SPEED,stepThree)
+    }
+
+    function stepThree() {
+        container.append($('#sliderBack > img').first());
+
+        $('#sliderBack > img').last()
+            .css({
+                'opacity': '0.5',
+                'width': '1920px',
+                'left': '1920px'
+            })
+            .delay(DELAY)
+            .animate({
+                left:'0',
+                opacity:'0.9'
+            },SPEED)
+            .animate({opacity:'1'},AFTER_SPEED,stepOne)
+    }
+}
+
+
+function scrollEff() {
+    if ($(document).width() > 1920)
+    {
+        $('#category > a').animate({left:'0'},800);
+        $('.note').animate({top: ''},600);
+    };
+
+    $(window).scroll(function ()
+    {
+        if ( $(window).scrollTop() > 800 )
+        {
+            $('#category > a').animate({left:'0'},800)
+        };
+
+        if( $(window).scrollTop() > 1200) $('.note').eq(0).animate({top: ''},600);
+        if( $(window).scrollTop() > 1400) $('.note').eq(1).animate({top: ''},600);
+        if( $(window).scrollTop() > 1600) $('.note').eq(2).animate({top: ''},600);
 
     });
+}
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
